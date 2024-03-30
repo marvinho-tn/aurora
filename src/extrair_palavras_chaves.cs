944da6af417d3da5f1621d4b3ed18d1370f8d3cf
marvinho-tn <marvinthomaz@gmail.com>
2024-03-30 23:52:05 +0000

    30 de mar. de 2024, 20:52

---
+++ src/extrair_palavras_chaves.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class KeywordExtractor
+{
+    public List<string> ExtrairPalavrasChave(string texto, int quantidadePalavras)
+    {
+        // Dividir o texto em palavras
+        string[] palavras = texto.Split(new[] { " ", ",", ".", ";", ":", "\n", "\r" }, StringSplitOptions.RemoveEmptyEntries);
+
+        // Contar a frequência de cada palavra
+        Dictionary<string, int> frequenciaPalavras = new Dictionary<string, int>();
+        foreach (string palavra in palavras)
+        {
+            string palavraLimpa = palavra.ToLower().Trim();
+            if (!frequenciaPalavras.ContainsKey(palavraLimpa))
+            {
+                frequenciaPalavras.Add(palavraLimpa, 1);
+            }
+            else
+            {
+                frequenciaPalavras[palavraLimpa]++;
+            }
+        }
+
+        // Ordenar as palavras por frequência
+        var palavrasOrdenadas = frequenciaPalavras.OrderByDescending(pair => pair.Value).Select(pair => pair.Key).ToList();
+
+        // Retornar as palavras-chave mais frequentes
+        return palavrasOrdenadas.Take(quantidadePalavras).ToList();
+    }
+}
