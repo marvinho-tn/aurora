b131177a7e06d1739efc96c7b83668850f8b657f
marvinho-tn <marvinthomaz@gmail.com>
2024-03-31 19:35:40 +0000

    31 de mar. de 2024, 16:35

--- src/config/ProjectConfigurations.cs
+++ src/config/ProjectConfigurations.cs
@@ -1,8 +1,5 @@
 using Aurora.Domain;
-using dotenv.net;
-
 using Microsoft.Extensions.DependencyInjection;
-
 
 namespace Aurora.Config
 {
@@ -27,36 +24,6 @@ namespace Aurora.Config
             return serviceProvider;
         }
     }
-
-    public static class DotEnvConfig
-    {
-        public static void Load(string filePath)
-        {
-            if (!File.Exists(filePath))
-                return;
-
-            foreach (var line in File.ReadAllLines(filePath))
-            {
-                var parts = line.Split(
-                    '=',
-                    StringSplitOptions.RemoveEmptyEntries);
-
-                if (parts.Length != 2)
-                    continue;
-
-                Environment.SetEnvironmentVariable(parts[0], parts[1]);
-            }
-        }
-
-        public static void Load()
-        {
-            var appRoot = Directory.GetCurrentDirectory();
-            var dotEnv = Path.Combine(appRoot, ".env");
-
-            Load(dotEnv);
-        }
-    }
-
 
     public interface IDependencyKeys
     {
