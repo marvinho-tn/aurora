a2431cbb829a31e757d20ba6ed3a265722578f91
marvinho-tn <marvinthomaz@gmail.com>
2024-04-01 02:55:50 +0000

    31 de mar. de 2024, 23:55

--- src/domain/models/memoria_bruta.cs
+++ src/domain/models/memoria_bruta.cs
@@ -2,6 +2,6 @@ namespace Aurora.Domain.Models
 {
     public class MemoriaBruta : Memoria
     {
-        public List<Categoria> Categorizacao { get; set; } = [];
+        public Categoria? Categoria { get; set; }
     }
 }
