0ec886b024cfbed2ce02e02c984884b3625c5bf0
marvinho-tn <marvinthomaz@gmail.com>
2024-04-03 14:51:12 +0000

    3 de abr. de 2024, 11:51

--- src/dominio/models/categoria_descricao.cs
+++ src/dominio/models/categoria_descricao.cs
@@ -3,6 +3,6 @@ namespace Aurora.Domain.Models
     public class DescriptionOfCategory : Category
     {
         public int DescriptionId { get; set; }
-        public string Value { get; set; } = string.Empty;
+        public string? Value { get; set; }
     }
 }
