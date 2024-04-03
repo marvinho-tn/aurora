961427414b03aad4ecfc45ca842cc4e45fa10ba2
marvinho-tn <marvinthomaz@gmail.com>
2024-04-03 12:11:07 +0000

    3 de abr. de 2024, 09:11

--- src/domain/models/resposta.cs
+++ src/domain/models/resposta.cs
@@ -2,6 +2,6 @@ namespace Aurora.Domain.Models
 {
     public class Resposta
     {
-        public  string Valor { get; set; } = string.Empty;
+        public  string? Valor { get; set; } = string.Empty;
     }
 }
