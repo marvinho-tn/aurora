fa26dd235a1aeda4215d9d7c9ab70c2b31aa4253
marvinho-tn <marvinthomaz@gmail.com>
2024-04-04 15:04:50 +0000

    4 de abr. de 2024, 12:04

--- src/dominio/modelos/evento.cs
+++ src/dominio/modelos/evento.cs
@@ -5,6 +5,7 @@ namespace Aurora.Domain.Models
         public object? From { get; set; }
         public object? Who { get; set; }
         public object? Type { get; set; }
+        public DateTime When { get; set; }
         public Func<object>? Action { get; set; }
     }
 }
