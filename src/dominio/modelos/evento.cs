e6c008469d83bb0e6ebad28a2292b78f1f7ae89d
marvinho-tn <marvinthomaz@gmail.com>
2024-04-04 15:13:23 +0000

    4 de abr. de 2024, 12:13

--- src/dominio/modelos/evento.cs
+++ src/dominio/modelos/evento.cs
@@ -2,10 +2,23 @@ namespace Aurora.Domain.Models
 {
     public class Event
     {
-        public object? From { get; set; }
-        public object? Who { get; set; }
-        public object? Type { get; set; }
-        public DateTime When { get; set; }
-        public Func<object>? Action { get; set; }
+        private Event(object from, object who, object type)
+        {
+            From = from;
+            Who = who;
+            Type = type;
+            When = DateTime.UtcNow;
+        }
+
+        private object? From { get; }
+        private object? Who { get; }
+        private object? Type { get; }
+        private DateTime When { get; }
+        private Action? Action { get; }
+
+        public void StartEvent(Action action)
+        {
+            
+        }
     }
 }
