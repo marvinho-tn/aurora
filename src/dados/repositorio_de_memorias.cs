4db8d811e9319a36fe798a26f2363a71271fca70
marvinho-tn <marvinthomaz@gmail.com>
2024-04-04 17:14:37 +0000

    4 de abr. de 2024, 14:14

--- src/dados/repositorio_de_memorias.cs
+++ src/dados/repositorio_de_memorias.cs
@@ -11,10 +11,26 @@ namespace Aurora.Data
     public class MemoryRepositoryv : IMemoryRepository
     {
         private static readonly SensibleMemory sensibleMemory = new();
-        
+        private static List<Comunication> ComunicationsFromMemony = new(sensibleMemory?.Comunications);
+
         public Comunication? TryGetResponseFromInput(string input)
         {
-            throw new NotImplementedException();
+            var id = 1;
+
+            if (ComunicationsFromMemony.Count != 0)
+                id = ComunicationsFromMemony.Count + 1;
+            
+            else
+                ComunicationsFromMemony = [];
+
+            var comunicationInput = ComunicationsFromMemony.FirstOrDefault(comunication => comunication.Register.ToString().Equals(input));
+            
+            if(comunicationInput.IsNotNull() && comunicationInput.Next.IsNotNull())
+                return comunicationInput.Next;
+
+            var comunication = new Comunication(id, input, Domain.Types.ComunicationType.Pergunta, null);
+
+            ComunicationsFromMemony.Add(comunication);
         }
     }
 }
