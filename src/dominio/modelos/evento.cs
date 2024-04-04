76f7e24b46d15502309c6e738a48fdc4f39c25aa
marvinho-tn <marvinthomaz@gmail.com>
2024-04-04 15:57:24 +0000

    4 de abr. de 2024, 12:57

--- src/dominio/modelos/evento.cs
+++ src/dominio/modelos/evento.cs
@@ -1,7 +1,11 @@
+using System.Reflection;
+
 namespace Aurora.Domain.Models
 {
     public class Event
     {
+        private static readonly List<Event> EventCollection = [];
+
         private Event(object from, object who, object type)
         {
             From = from;
@@ -15,6 +19,7 @@ namespace Aurora.Domain.Models
         private object Type { get; }
         private DateTime When { get; }
         private Action? Action { get; set; }
+        private Event? Consequence { get; set; }
 
         public void StartEvent(object from, object who, object type, Action action)
         {
@@ -22,6 +27,8 @@ namespace Aurora.Domain.Models
             {
                 Action = action
             };
+
+            EventCollection.Add(_event);
 
             if (Action.IsNotNull())
                 Action();
@@ -36,14 +43,28 @@ namespace Aurora.Domain.Models
 
             try
             {
-                _event.StartEvent(from, who, type, action);
+                _event.StartEvent(_event.From, _event.Who, _event.Type, _event.Action);
             }
             catch(Exception ex)
             {
+                var methodName = "TryStartEvent";
+                
+                object? _from = typeof(Event).GetMethod("TryStartEvent");
+
+                if(_from.IsNull())
+                    _from = methodName;
+
+                _event.Consequence = TryStartEvent(_from, _event, typeof(Event), () => ex.GetBaseException());
+
                 return _event;
             }
 
             return _event;
+        }
+
+        public static void AddEventToCollection(Event _event)
+        {
+            EventCollection.Add(_event);
         }
     }
 }
