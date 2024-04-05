00e0987c91aa8262b05083dfa2156e153eb761c6
marvinho-tn <marvinthomaz@gmail.com>
2024-04-05 00:38:10 +0000

    4 de abr. de 2024, 21:38

--- src/dominio/modelos/comunicacao.cs
+++ src/dominio/modelos/comunicacao.cs
@@ -3,10 +3,11 @@ using Aurora.Domain.Types;
 
 namespace Aurora.Domain.Models
 {
-    public class Comunication(int id, object register, ComunicationType type)
+    public class Comunication(int id, object register, string who, ComunicationType type)
     {
         public int Id { get; set; } = id;
         public object Register { get; set; } = register;
+        public string Who { get; set; } = who;
         public ComunicationType Type { get; set; } = type;
         public DateTime When { get; set; } = DateTime.UtcNow;
 
