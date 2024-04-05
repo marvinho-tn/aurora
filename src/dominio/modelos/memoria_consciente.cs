dd6c0c0c87d57d347a085129a6b518ff096b225c
marvinho-tn <marvinthomaz@gmail.com>
2024-04-05 18:22:25 +0000

    5 de abr. de 2024, 15:22

--- src/dominio/modelos/memoria_consciente.cs
+++ src/dominio/modelos/memoria_consciente.cs
@@ -2,6 +2,6 @@ namespace Aurora.Domain.Models
 {
     public class SensibleMemory
     {
-        public List<Comunication> Dialogs { get; set; } = [];
+        public List<Comunication> Comunications { get; set; } = [];
     }
 }
