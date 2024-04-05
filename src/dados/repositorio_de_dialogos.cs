813b90336ecc19f857bd2a00bb511849e7a9bfe4
marvinho-tn <marvinthomaz@gmail.com>
2024-04-05 16:27:15 +0000

    5 de abr. de 2024, 13:27

--- src/dados/repositorio_de_dialogos.cs
+++ src/dados/repositorio_de_dialogos.cs
@@ -26,70 +26,7 @@ namespace Aurora.Data
 
         public Comunication? GetComunicationResponse(Comunication request)
         {
-            foreach (var dialog in Dialogs)
-            {
-                if (dialog.Comunications.Count > 2)
-                {
-                    var comunication = GetNextComunicationFromDialog(request, dialog);
-
-                    if (comunication.IsNull())
-                        continue;
-                }
-            }
-
-            return null;
-        }
-
-        private static Comunication? GetNextComunicationFromDialog(Comunication request, Dialog dialog)
-        {
-            foreach (var comunication in dialog.Comunications)
-            {
-                var response = GetPreviousComunication(dialog, comunication, Constants.IDontKnowWhaISay, request);
-
-                if (response.IsNull())
-                    response = GetNextComunication(dialog, comunication, request.Register, request);
-                if (response.IsNotNull())
-                    return response;
-            }
-
-            return null;
-        }
-
-        private static Comunication? GetNextComunication(Dialog dialog, Comunication comunication, object message, Comunication request)
-        {
-            if (request.Register.Equals(message))
-            {
-                var index = dialog.Comunications.IndexOf(comunication);
-
-                index++;
-
-                if (dialog.Comunications.Count > index)
-                    return dialog.Comunications[index];
-            }
-
-            return null;
-        }
-
-        private static Comunication? GetPreviousComunication(Dialog dialog, Comunication comunication, object message, Comunication request)
-        {
-            if (message.Equals(Constants.IDontKnowWhaISay))
-            {
-                var dontknow = dialog.Comunications.Where(comunication => comunication.Equals(Constants.IDontKnowWhaISay));
-
-                foreach (var c in dontknow)
-                {
-                    foreach (var d in dialog.Comunications)
-                    {
-                        if (comunication.Id.Equals(d.Id))
-                        {
-
-                        }
-                    }
-                }
-            }
-
-
-            return null;
+            return request;
         }
     }
 }
