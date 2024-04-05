9eec9fb741537deca935966848b74dc3628c54e6
marvinho-tn <marvinthomaz@gmail.com>
2024-04-05 02:35:20 +0000

    4 de abr. de 2024, 23:35

--- src/Program.cs
+++ src/Program.cs
@@ -14,13 +14,11 @@ namespace Aurora
 
         static void Main(string[] args)
         {
-            EventFromUser = new Event("Usuário do Console Applicaion", "Class Program, Method Main", EventType.Dialog, StartConversationFromUser);
+            EventFromUser = new Event("Usuário do Console Applicaion", "Class Program, Method Main", EventType.Dialog, StartConversationFromUser, EventFromProgram);
             EventFromProgram = new Event("Dotnet Console Application", "Class Program, Method Main", EventType.Dialog, StartConversationFromProgram)
             {
                 Consequence = EventFromUser
             };
-
-            EventFromUser.Consequence = EventFromProgram;
 
             EventFromProgram.Start();
         }
