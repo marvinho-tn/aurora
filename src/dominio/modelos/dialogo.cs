863fd4a9f693d5f49c6046313bb1a4c752d36e78
marvinho-tn <marvinthomaz@gmail.com>
2024-04-06 16:39:34 +0000

    6 de abr. de 2024, 13:39

--- src/dominio/modelos/dialogo.cs
+++ src/dominio/modelos/dialogo.cs
@@ -19,10 +19,8 @@ namespace Aurora.Domain.Models
             var messageOne = CreateMessage(value.Item1, previous?.Item1);
             var messageTwo = CreateMessage(value.Item2, previous?.Item2);
 
-            messageOne.Previous = previous?.Item1;
             messageOne.Previous = previous?.Item2;
             messageTwo.Previous = previous?.Item1;
-            messageTwo.Previous = previous?.Item2;
 
             Current = new Tuple<Message, Message>(messageOne, messageTwo);
 
