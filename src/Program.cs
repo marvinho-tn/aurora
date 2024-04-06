b0b9a7fe8c76d5bfc8296edda80f72b5ef0e48f2
marvinho-tn <marvinthomaz@gmail.com>
2024-04-06 16:07:00 +0000

    6 de abr. de 2024, 13:07

--- src/Program.cs
+++ src/Program.cs
@@ -10,29 +10,48 @@ namespace Aurora
 {
     class Program
     {
-        public static Message? CurrentMessage = default;
+        public static Message? CurrentMessageMonolog = default;
+        public static Tuple<Message, Message>? CurrentMessageDialog = default;
         public static Event? CurrentEvent = default;
 
         static void Main(string[] args)
         {
             string[] from = ["Monolog", "StartConversation", "Method Main", "Class Program", "Namespace Auroora", "Console Application", "csharp", "Dotnet core 8", "visual studio code", "macos 17"];
 
-            var myName = "Marvin";
+            var requestAuthor = "Marvin";
+            var respopnseAuthor = "Aurora";
+            var authors = (requestAuthor, respopnseAuthor);
 
-            CurrentEvent = new Event(from, myName, EventType.Dialog, () => StartConversation(myName));
+            CurrentEvent = new Event(from, authors, EventType.Dialog, () => StartDialog(authors));
             CurrentEvent.Consequence = CurrentEvent;
             CurrentEvent.Start();
         }
 
-        static void StartConversation(string author)
+        static void StartDialog((string, string) authors)
         {
             var serviceProvider = DependencyConfiguration.Configure();
-            var comunication = serviceProvider.GetService<IComunicationService>();
+            var service = serviceProvider.GetService<IComunicationService>();
+            var request = Console.ReadLine();
+            var response = Console.ReadLine();
+            var message = (request, response);
+
+            CurrentMessageDialog = service.MakeFirstOrNextDialog(message, authors, CurrentMessageDialog);
+
+            var json = JsonSerializer.Serialize(CurrentMessageMonolog);
+
+            Console.Clear();
+            Console.WriteLine(json);
+        }
+
+        static void StartMonolog(string author)
+        {
+            var serviceProvider = DependencyConfiguration.Configure();
+            var service = serviceProvider.GetService<IComunicationService>();
             var message = Console.ReadLine();
 
-            CurrentMessage = comunication.MakeFirstOrNextMonolog(message, author, CurrentMessage);
+            CurrentMessageMonolog = service.MakeFirstOrNextMonolog(message, author, CurrentMessageMonolog);
 
-            var json = JsonSerializer.Serialize(CurrentMessage);
+            var json = JsonSerializer.Serialize(CurrentMessageMonolog);
 
             Console.Clear();
             Console.WriteLine(json);
--- src/dominio/modelos/evento.cs
+++ src/dominio/modelos/evento.cs
@@ -2,10 +2,10 @@ using Aurora.Domain.Types;
 
 namespace Aurora.Domain.Models
 {
-    public class Event(string[] from, string author, EventType type, Action action, Event? consequence = null)
+    public class Event(string[] from, object author, EventType type, Action action, Event? consequence = null)
     {
         public string[] From { get; } = from;
-        public string Author { get; } = author;
+        public object Author { get; } = author;
         public EventType Type { get; } = type;
         public DateTime Created { get; } = DateTime.UtcNow;
         public Action Action { get; set; } = action;
