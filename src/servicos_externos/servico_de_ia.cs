cbc0b3b1186c26ae665972450fa1d1f9b2e1efd7
marvinho-tn <marvinthomaz@gmail.com>
2024-04-07 16:50:59 +0000

    7 de abr. de 2024, 13:50

--- src/servicos_externos/servico_de_ia.cs
+++ src/servicos_externos/servico_de_ia.cs
@@ -1,4 +1,4 @@
-using Microsoft.Bot.Builder;
+using Aurora.Configuration;
 using Microsoft.Bot.Builder.AI.Luis;
 using Microsoft.Bot.Schema;
 
@@ -13,17 +13,12 @@ namespace Aurora.ExternalServices
     {
         public async Task<string> Dialog(string text)
         {
-            var luisRecognizer = new LuisRecognizer(new LuisApplication
-            {
-                ApplicationId = keyService.Get(CYOUR_LUIS_APP_ID"),
-                SubscriptionKey = keyService.Get(CYOUR_LUIS_SUBSCRIPTION_KEY"),
-                Region = keyService.Get(CYOUR_LUIS_REGION)"
-            });
+            var application = new LuisApplication();
+            var options = new LuisRecognizerOptionsV3(application);
+            var recognizer = new LuisRecognizer(options);
+            var result = await recognizer.RecognizeAsync(text);
 
-            var activity = new Activity(text);
-            var result = await recognizer.RecognizeAsync(activity);
-
-            return result;
+            return result?.Text;
         }
     }
 }
