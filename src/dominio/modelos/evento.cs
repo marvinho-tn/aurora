29d02a0ac8cc40456422b091b9b39f438a88041b
marvinho-tn <marvinthomaz@gmail.com>
2024-04-08 12:56:46 +0000

    8 de abr. de 2024, 09:56

--- src/dominio/modelos/evento.cs
+++ src/dominio/modelos/evento.cs
@@ -1,12 +1,10 @@
-using Aurora.Domain.Types;
-
 namespace Aurora.Domain.Models
 {
-    public class Event(string[] from, object author, EventType type, Action action, Event? consequence = null)
+    public class Event(string[] from, object author, object type, Action action, Event? consequence = null)
     {
         public string[] From { get; } = from;
         public object Author { get; } = author;
-        public EventType Type { get; } = type;
+        public object Type { get; } = type;
         public DateTime Created { get; } = DateTime.UtcNow;
         public Action Action { get; set; } = action;
         public Event? Consequence { get; set; } = consequence;
