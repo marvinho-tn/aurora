2339db934bfa77ba92f0f595a258f50fc523de33
marvinho-tn <marvinthomaz@gmail.com>
2024-04-08 12:37:24 +0000

    8 de abr. de 2024, 09:37

--- src/Program.cs
+++ src/Program.cs
@@ -10,8 +10,8 @@ namespace Aurora
 {
     class Program
     {
-        public static object? CurrentMessage = default;
-        public static Event? CurrentEvent = default;
+        public static object CurrentMessage = "O que nós podemos fazer para entender melhor como criar consciencia na tecnologia?";
+        public static Event CurrentEvent = new(From, Author, EventType.Dialog, () => Task.FromResult(Start()));
         public static IServiceProvider ServiceProvider = DependencyConfiguration.Configure();
         public static string[] From = ["Monolog", "StartConversation", "Method Main", "Class Program", "Namespace Auroora", "Console Application", "csharp", "Dotnet core 8", "visual studio code", "macos 17"];
         public static IComunicationService ComunicationService = ServiceProvider.GetRequiredService<IComunicationService>();
@@ -20,17 +20,12 @@ namespace Aurora
 
         static void Main(string[] args)
         {
-            
-            CurrentEvent = new Event(From, Author, EventType.Dialog, () => Task.FromResult(Start()));
             CurrentEvent.Consequence = CurrentEvent;
             CurrentEvent.Start();
         }
 
         static async Task Start()
         {
-            if(CurrentMessage.IsNull())
-                CurrentMessage = "O que nós podemos fazer para entender melhor como criar consciencia na tecnologia?";            
-            
             var result = await IAService.Dialog(CurrentMessage.As<string>());
 
             CurrentMessage = $"o você acha dessa declaração \"{result}\"?";
