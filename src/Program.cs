35409299e0b20fd07cdfaa182735612858b680c0
marvinho-tn <marvinthomaz@gmail.com>
2024-04-08 01:38:08 +0000

    7 de abr. de 2024, 22:38

--- src/Program.cs
+++ src/Program.cs
@@ -13,6 +13,7 @@ namespace Aurora
         public static object? CurrentMessage = default;
         public static Event? CurrentEvent = default;
         public static IServiceProvider ServiceProvider = DependencyConfiguration.Configure();
+        public static string[] From = ["Monolog", "StartConversation", "Method Main", "Class Program", "Namespace Auroora", "Console Application", "csharp", "Dotnet core 8", "visual studio code", "macos 17"];
 
         static void Main(string[] args)
         {
@@ -20,9 +21,21 @@ namespace Aurora
 
             var requestAuthor = "Marvin";
             var respopnseAuthor = "Aurora";
-            var authors = (requestAuthor, respopnseAuthor);
+            var authors = (requestAuthor, respopnseAuthor); 
 
-            CurrentEvent = new Event(from, authors, EventType.Dialog, async () => await StartIAMonolog(requestAuthor));
+            Console.WriteLine("Qual o tipo de dialogo? (1 - monologo, 2 - dialogo, 3 - IA)");
+
+            var comunicationType = Console.ReadLine();
+            var action = default(Action);
+
+            switch (comunicationType.As<int>())
+            {
+                case 1: action = () => StartMonolog(requestAuthor); break;
+                case 2: action = () => StartDialog(authors); break;
+                case 3: action = async () => await StartIAMonolog(respopnseAuthor); break;
+            }
+
+            CurrentEvent = new Event(from, authors, EventType.Dialog, action);
             CurrentEvent.Consequence = CurrentEvent;
             CurrentEvent.Start();
         }
