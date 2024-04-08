df36cffae556fd1fb6fbd654907ce494c2a77dfc
marvinho-tn <marvinthomaz@gmail.com>
2024-04-08 13:06:15 +0000

    8 de abr. de 2024, 10:06

--- src/dominio/modelos/causa.cs
+++ src/dominio/modelos/causa.cs
@@ -1,11 +1,7 @@
 namespace Aurora.Domain.Models
 {
-    public class Cause(string[] from, object author, object type, Action action, Consequence? consequence = null)
+    public class Cause(Action action, Consequence? consequence = null)
     {
-        public string[] From { get; } = from;
-        public object Author { get; } = author;
-        public object Type { get; } = type;
-        public DateTime Created { get; } = DateTime.UtcNow;
         public Action Action { get; set; } = action;
         public Cause? Consequence { get; set; } = consequence;
 
