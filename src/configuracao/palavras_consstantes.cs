80155bce5ad002ebde066af1d56f5a1de29e2226
marvinho-tn <marvinthomaz@gmail.com>
2024-04-08 03:34:44 +0000

    8 de abr. de 2024, 00:34

--- src/configuracao/palavras_consstantes.cs
+++ src/configuracao/palavras_consstantes.cs
@@ -2,8 +2,7 @@ namespace Aurora.Configuration
 {
     public static class AuroraConstantsWords
     {
-        public const string LUIS_APP_ID = "LUIS_APP_ID";
-        public const string AZURE_CREDENTIALS_KEY = "LUIS_SUBSCRIPTION_KEY";
-        public const string AZURE_AI_URI = "LUIS_URI";
+        public const string AZURE_CREDENTIALS_KEY = "AZURE_CREDENTIALS_KEY";
+        public const string AZURE_AI_URI = "AZURE_AI_URI";
     }
 }
