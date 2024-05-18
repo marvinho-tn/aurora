89b3416b8ad8ec6461da35488eca3a33338f2590
marvinho <163774456+marvinho-tn@users.noreply.github.com>
2024-05-18 12:29:18 +0000

    May 18, 2024, 12:29 PM

--- src/AppServices/AIService.cs
+++ src/AppServices/AIService.cs
@@ -1,9 +1,10 @@
 using Aurora.Configuration;
+using HuggingFace;
 using OpenAI_API;
 
 namespace Aurora.AppServices
 {
-    public interface IAIService
+	public interface IAIService
 	{
 		string Dialog(string text);
 		Task<string> DialogAsync(string text);
@@ -11,14 +12,14 @@ namespace Aurora.AppServices
 
 	public class OpenAIService(IKeyProvider keyProvider) : IAIService
 	{
-        private readonly IKeyProvider _keyProvider = keyProvider;
+		private readonly IKeyProvider _keyProvider = keyProvider;
 
-        public string Dialog(string text)
+		public string Dialog(string text)
 		{
 			return Task.Run(() => DialogAsync(text)).Result;
 		}
 
-        public async Task<string> DialogAsync(string text)
+		public async Task<string> DialogAsync(string text)
 		{
 			var apiKey = _keyProvider.Get(AuroraConstants.OPEN_API_KEY);
 			var api = new OpenAIAPI(apiKey);
@@ -29,18 +30,36 @@ namespace Aurora.AppServices
 
 	public class HuggingFaceAIService(IKeyProvider keyProvider) : IAIService
 	{
-        private readonly IKeyProvider _keyProvider = keyProvider;
+		private readonly IKeyProvider _keyProvider = keyProvider;
 
-        public string Dialog(string text)
+		public string Dialog(string text)
 		{
 			return Task.Run(() => DialogAsync(text)).Result;
 		}
 
-        public async Task<string> DialogAsync(string text)
+		public async Task<string> DialogAsync(string text)
 		{
 			var apiKey = _keyProvider.Get(AuroraConstants.OPEN_API_KEY);
-			var api = new OpenAIAPI(apiKey);
-			var response = await api.Completions.CreateCompletionAsync(text);
+			var client = new HttpClient();
+			var api = new HuggingFaceApi(apiKey, client);
+			var response = await api.GenerateTextAsync(
+				RecommendedModelIds.Gpt2,
+				new GenerateTextRequest
+				{
+					Inputs = "Hello",
+					Parameters = new GenerateTextRequestParameters
+					{
+						Max_new_tokens = 250,
+						Return_full_text = false,
+					},
+					Options = new GenerateTextRequestOptions
+					{
+						Use_cache = true,
+						Wait_for_model = false,
+					},
+				}
+			);
+
 			return response.ToString();
 		}
 	}
