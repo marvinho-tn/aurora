e85cf764409746c44b1131a2ff1883e6f66e12b2
marvinho <163774456+marvinho-tn@users.noreply.github.com>
2024-05-18 12:23:54 +0000

    May 18, 2024, 12:23 PM

--- src/AppServices/AIService.cs
+++ src/AppServices/AIService.cs
@@ -26,4 +26,22 @@ namespace Aurora.AppServices
 			return response.ToString();
 		}
 	}
+
+	public class HuggingFaceAIService(IKeyProvider keyProvider) : IAIService
+	{
+        private readonly IKeyProvider _keyProvider = keyProvider;
+
+        public string Dialog(string text)
+		{
+			return Task.Run(() => DialogAsync(text)).Result;
+		}
+
+        public async Task<string> DialogAsync(string text)
+		{
+			var apiKey = _keyProvider.Get(AuroraConstants.OPEN_API_KEY);
+			var api = new OpenAIAPI(apiKey);
+			var response = await api.Completions.CreateCompletionAsync(text);
+			return response.ToString();
+		}
+	}
 }
